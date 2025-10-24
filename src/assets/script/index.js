236ac58dbf282f9292d519274519d7c985d3a714
Nguyen Hoang <cunpoi1357@gmail.com>
2025-10-24 16:07:19 +0700

    refactor: Base dự án

---
+++ src/assets/script/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('btn');
+    btn.addEventListener('click', () => {
+        alert('Hello');
+    });
+});
